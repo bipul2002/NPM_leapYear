6631ec5f32b569a73d8a2faa1e417b00ff2b2dc6
snnehasis <bipuldebyans@gmail.com>
2024-04-03 13:38:46 +0530

    gitignore working

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Leap {
-    static isLeapYear(year: number): boolean;
-    static displayLeapYear(year: number): void;
-    static displayLeapYearsInRange(startYear: number, endYear: number): void;
-    static nextLeapYear(year: number): number | undefined;
-}
